e90b85c9094292eff9c79f7a30ebf64866c8e393
Shajin <shajin@izaaptech.com>
2023-12-05 21:15:03 +0530

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,5 +1,5 @@
 #print("I love you abinaya, I love di pondatii")
-height = input(int("Enter the height in cm"))
+height = int(input("Enter the height in cm:  "))
 if height >= 170:
   print("He is eligible")
 else:
