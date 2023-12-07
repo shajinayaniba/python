441eef38e548e26052a2d591c4b3f883d7abfda9
Shajin <shajin@izaaptech.com>
2023-12-07 19:56:29 +0530

    Update python3.py

--- python3.py
+++ python3.py
@@ -27,16 +27,16 @@
 #  print("Not eligible") 
 # 
 #  BMI Calculator
-# height = float(input("enter your height in m: "))
-# weight = float(input("enter your weight in kg: "))
-# bmi = round(weight / height **2)
-# if bmi < 18.5:
-#  print(f"your bmi is {bmi} ,you are underweight.")
-# elif bmi < 25:
-#  print(f"your bmi is {bmi} ,you are normalweight.")
-# elif bmi < 30:
-#  print(f"your bmi is {bmi} ,you are overweight.")
-# elif bmi < 35:
-#  print(f"your bmi is {bmi} ,you are obese.")
-# else:
-#  print(f"your bmi is {bmi} ,you are clinically obeseee.")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+bmi = round(weight / height **2)
+if bmi < 18.5:
+ print(f"your bmi is {bmi} ,you are underweight.")
+elif bmi < 25:
+ print(f"your bmi is {bmi} ,you are normalweight.")
+elif bmi < 30:
+ print(f"your bmi is {bmi} ,you are overweight.")
+elif bmi < 35:
+ print(f"your bmi is {bmi} ,you are obese.")
+else:
+ print(f"your bmi is {bmi} ,you are clinically obeseee.")
