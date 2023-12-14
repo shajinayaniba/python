9b8b88bcca06dd5fbe35b32ae232d26894c2b4ed
Shajin <shajin@izaaptech.com>
2023-12-14 21:37:38 +0530

    Update python4.py

--- python4.py
+++ python4.py
@@ -1,4 +1,10 @@
-import random
-random_integer =random.randint(1, 10)
-print(random_integer)
-
+# import random
+# random_integer =random.randint(1, 10)
+# print(random_integer)
+#-----------
+# import random
+# random_test = random.randint(0, 1)
+# if random_test == 1:
+#     print("Head")
+# else:
+#     print("Tail")
