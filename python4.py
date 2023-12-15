171c89b5c7867cde0defb99b90318ad4b782c322
Shajin <shajin@izaaptech.com>
2023-12-15 20:21:39 +0530

    Update python4.py

--- python4.py
+++ python4.py
@@ -8,3 +8,16 @@
 #     print("Head")
 # else:
 #     print("Tail")
+
+# row1 =["*", "*", "*"]
+# row2 =["*", "*", "*"]
+# row3 =["*", "*", "*"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}\n")
+# position = input("Enter where you want to save the treasure")
+
+# horizontal = int(position[0])
+# vertical = int(position[1])
+
+# map[vertical -1][horizontal -1] = "X"
+# print(f"{row1}\n{row2}\n{row3}\n")
