98bec48d1390a4fc17ce0b79be12eebc3100dd05
Shajin <shajin@izaaptech.com>
2023-12-19 19:27:59 +0530

    Update python5.py

--- python5.py
+++ python5.py
@@ -2,3 +2,13 @@ student_height = input("Input the list of students height").split()
 for n in range(0, len(student_height)):
     student_height[n] = int(student_height[n])
 print(student_height)
+
+total_height =0
+for height in total_height:
+    total_height += height
+    print(total_height)
+
+number_of_students = 0
+for student in student_height:
+    number_of_students += 1
+    print(number_of_students)    
