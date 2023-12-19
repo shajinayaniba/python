a17d17c9d976b8de7f236b15510af9259c348358
Shajin <shajin@izaaptech.com>
2023-12-19 20:34:12 +0530

    Update python5.py

--- python5.py
+++ python5.py
@@ -1,14 +1,28 @@
-student_height = input("Input the list of students height ").split()
-for n in range(0, len(student_height)):
-    student_height[n] = int(student_height[n])
-print(student_height)
+# student_height = input("Input the list of students height ").split()
+# for n in range(0, len(student_height)):
+#     student_height[n] = int(student_height[n])
+# print(student_height)
+ 
+# total_height = 0
+# for height in student_height:
+#     total_height += height
+# print(total_height)
 
-total_height =0
-for height in total_height:
-    total_height += height
-print(total_height)
+# number_of_students = 0
+# for student in student_height:
+#     number_of_students += 1
+# print(number_of_students)    
+#---------------------------------------------
+# student_scores = input("enter the list of scores for who got highest").split()
+# for n in range(0, len(student_scores)):
+#     student_scores[n] = int(student_scores[n])
+# print(student_scores)
 
-number_of_students = 0
-for student in student_height:
-    number_of_students += 1
-print(number_of_students)    
+# highest_score = 0
+# for score in student_scores:
+#     if score > highest_score:
+#      highest_score = score
+# print(highest_score)
+
+
+
