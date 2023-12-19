4a5d604cee53d99b5f246ea5e6a90592d0c31de1
Shajin <shajin@izaaptech.com>
2023-12-19 19:33:38 +0530

    Update python5.py

--- python5.py
+++ python5.py
@@ -1,4 +1,4 @@
-student_height = input("Input the list of students height").split()
+student_height = input("Input the list of students height ").split()
 for n in range(0, len(student_height)):
     student_height[n] = int(student_height[n])
 print(student_height)
@@ -6,9 +6,9 @@ print(student_height)
 total_height =0
 for height in total_height:
     total_height += height
-    print(total_height)
+print(total_height)
 
 number_of_students = 0
 for student in student_height:
     number_of_students += 1
-    print(number_of_students)    
+print(number_of_students)    
