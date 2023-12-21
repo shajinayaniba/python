ebde9bedd1c89ead848bfd27fa7f2c57a1ce65e8
Shajin <shajin@izaaptech.com>
2023-12-21 20:32:47 +0530

    Update python5.py

--- python5.py
+++ python5.py
@@ -29,6 +29,20 @@
 # for number in range(1, 10):
 #     total += number
 # print(total)
+#-------------------sum of all even numbers
+
+# total = 0
+# for number in range(2, 101, 2):
+#     total += number
+# print(total)
+
+# to_tal = 0
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#       to_tal += number
+# print(to_tal)
+#-----------------------
+
 
 
 
