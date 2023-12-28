a58704638765a860c0585ee6cb358efa9dccec4d
Shajin <shajin@izaaptech.com>
2023-12-28 21:01:41 +0530

    Update python7_hangman.py

--- python7_hangman.py
+++ python7_hangman.py
@@ -1,1 +1,10 @@
+import random
+word_list = ["abinaya", "shajin"]
+chosen_word = random.choice(word_list)
+guess = input("Enter the word: ").lower
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("wrong")
 
