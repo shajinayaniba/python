07000f16b79ca3a9e6cd5094aaee82f21448a5bc
Shajin <shajin@izaaptech.com>
2024-02-05 20:15:13 +0530

    Update python8.py

--- python8.py
+++ python8.py
@@ -12,15 +12,35 @@
 
 # PRIME NUMBER CHECKER
 
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-    if is_prime:
-        print("Its prime")
-    else:
-        print("not prime")
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print("Its prime")
+#     else:
+#         print("not prime")
 
-n = int(input("check this number :"))
-prime_checker(number=n)
+# n = int(input("check this number :"))
+# prime_checker(number=n)
+
+
+
+#Ceaser cipher
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Encode or Decode")
+text = input("Typer your message").lower()
+shift = int(input("Type the shift number"))
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        postion = alphabet.index(letter)
+        new_positon = postion + shift_amount
+        new_letter = alphabet[new_positon]
+        cipher_text += new_letter
+    print(f"The encoded code is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
